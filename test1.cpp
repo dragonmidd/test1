7e9baf3e43db91e7f178959f3f95c6de72dbb113
dragonmidd <dragonmidd@gmail.com>
2017-04-20 15:18:06 +0800

    add test_windows more

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,11 @@
-#include "test_boost.h"
-#include "test_asm.h"
-#include "test_cpp11.h"
+// #include "test_boost.h"
+// #include "test_asm.h"
+// #include "test_cpp11.h"
 #include "test_stl.h"
-#include "test_queue.h"
+//#include "test_queue.h"
+//#include "test_multithread.h"
+#include "test_cpp11_future.h"
+#include "test_windows.h"
 
 int main(int argc, char** argv)
 {
@@ -14,7 +17,13 @@ int main(int argc, char** argv)
 
 //	test_stl();
 
-	test_queue();
+//	test_queue();
+
+//	test_multithread();
+
+//	test_cpp11_future();
+
+	test_windows();
 
 	getchar();
 	return 0;
